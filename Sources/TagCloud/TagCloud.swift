4e69cca4bb9140117435685f840c81d950371fad
Yaroslav Spirin <yar.spirin@gmail.com>
2023-07-17 00:00:51 +0100

    Set up package

---
+++ Sources/TagCloud/TagCloud.swift
@@ -0,0 +1,6 @@
+public struct TagCloud {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/TagCloudTests/TagCloudTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TagCloud
+
+final class TagCloudTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TagCloud().text, "Hello, World!")
+    }
+}
