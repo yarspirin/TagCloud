81c1019541c2748cfecff4eabc05ca0bdcaa3713
Yaroslav Spirin <yar.spirin@gmail.com>
2023-07-20 15:55:15 +0100

    Update tests

--- Tests/TagCloudTests/TagCloudTests.swift
+++ Tests/TagCloudTests/TagCloudTests.swift
@@ -8,16 +8,17 @@ fileprivate struct Tag {
 }
 
 final class TagCloudTests: XCTestCase {
-  func testFlowLayoutViewCreation() {
+  
+  func testDefaultTagCloudViewCreation() {
+    let tagCloudView = TagCloudView(tags: ["Hello", "World", "I", "love", "Swift"])
+    XCTAssertNotNil(tagCloudView)
+  }
+  
+  func testCustomTagCloudViewCreation() {
     let tags = (1 ... 5).map { Tag(num: $0) }
     let flowLayoutView = TagCloudView(data: tags) { tag in
       Text("\(tag.num)")
     }
     XCTAssertNotNil(flowLayoutView)
-  }
-  
-  func testTagCloudViewCreation() {
-    let tagCloudView = TagCloudView(tags: ["Hello", "World", "I", "love", "Swift"])
-    XCTAssertNotNil(tagCloudView)
   }
 }
